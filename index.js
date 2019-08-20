75880ff25b35c4ab57b26e7da9dbb6ad9c10b141
Drew Miley <drew.miley@komododigital.co.uk>
2019-08-20 12:19:00 +0100

    Step 4 code

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const fetch = require('node-fetch');
 
-let results = [];
-
-async function fetchEmails(minimumAge) {
-    const response = await fetch('https://randomuser.me/api/?results=10');
-    const jsonResponse = await response.json();
-    const users = jsonResponse.results;
-    const emails = users
-        .filter(user => user.dob.age > minimumAge)
-        .map(user => user.email);
-    results = results.concat(emails);
-    return;
+async function fetchEmails(minimumAge = 30) {
+  const response = await fetch('https://randomuser.me/api/?results=10');
+  const jsonResponse = await response.json();
+  return jsonResponse.results
+      .filter(user => user.dob.age > minimumAge)
+      .map(user => user.email);
 }
 
 async function fetchEmailsMultipleTimes(timesToCall, minimumAge) {
-    await Promise.all(Array.from(Array(timesToCall).keys())
+    return await Promise.all(Array.from(Array(timesToCall).keys())
         .map(async () => {
-            await fetchEmails(minimumAge)
-            return;
+            return await fetchEmails(minimumAge);
         }));
-    return;
 }
 
 async function run() {
-  await fetchEmailsMultipleTimes(5, 65);
-  console.log(results);
+    const results = await fetchEmailsMultipleTimes(5, 65);
+    console.log(results.flat());
 }
 
 run();
